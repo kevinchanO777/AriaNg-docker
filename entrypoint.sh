212926b51aa6b13743206cead282b8b54ebddf64
Kevin HM Chan <kevinchan714456@gmail.com>
2026-05-19 23:07:29 +0800

    aria2c: increase --max-tries
    remove unknown flags those only appear in custom built aria2c binary

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+LOG_FILE="/tmp/aria2.log"
+
 mkdir -p /downloads
 echo "Starting aria2c..."
 aria2c --enable-rpc --rpc-listen-all --rpc-allow-origin-all \
@@ -6,18 +9,18 @@ aria2c --enable-rpc --rpc-listen-all --rpc-allow-origin-all \
   --dir=/downloads \
   --continue=true --max-connection-per-server=16 \
   --min-split-size=1M --split=16 \
-  --max-tries=20 \
-  --retry-wait=30 \
-  --retry-on-400=true \
-  --retry-on-403=true \
-  --retry-on-404=true \
-  --retry-on-unknown=true >/tmp/aria2.log 2>&1 &
+  --max-file-not-found=10 \
+  --max-tries=100 \
+  --retry-wait=10 \
+  --log=$LOG_FILE --log-level=warn \
+  --daemon
+
 sleep 1
 if pgrep aria2c >/dev/null 2>&1; then
   echo "aria2c started on port 6800"
 else
   echo "aria2c failed to start. Logs:"
-  cat /tmp/aria2.log
+  cat $LOG_FILE
 fi
 echo "Starting HTTP server on port 9222..."
 python3 -m http.server 9222 -d /app
